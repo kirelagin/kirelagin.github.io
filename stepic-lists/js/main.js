4d7aa797152933c6b346139274d34f9d4da5d696
Kirill Elagin <kirelagin@gmail.com>
2016-04-27 00:19:14 +0300

    stepic-lists: init

---
+++ stepic-lists/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+        $('.tooltip').tooltipster({
+                theme: 'tooltipster-noir',
+                position: 'top',
+                animation: 'fade',
+                delay: 0,
+                speed: 600,
+                //positionTracker: true,
+                touchDevices: false,
+                updateAnimation: false,
+                contentAsHTML: true
+        });
+});
